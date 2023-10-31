4ae62289687ef2a6a24d988dfea20bd3fc292916
Pablo Vicente Munuera <pablovm1990@gmail.com>
2023-10-31 13:10:46 +0000

    tris

---
+++ src/pyVertexModel/tris.py
@@ -0,0 +1,8 @@
+class Tris:
+    def __init__(self):
+        self.Edge = []
+        self.SharedByCells = []
+        self.EdgeLength = []
+        self.LengthsToCentre = []
+        self.AspectRatio = []
+        self.EdgeLength_time = []
