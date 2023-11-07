847d4feacd77775720c7e7be12de91eaf36030fd
Pablo Vicente Munuera <pablovm1990@gmail.com>
2023-11-07 16:59:54 +0000

    contractility bugfix

--- src/pyVertexModel/Kg/kgContractility.py
+++ src/pyVertexModel/Kg/kgContractility.py
@@ -34,7 +34,7 @@ class KgContractility(Kg):
                             #currentFace.Tris.ContractileG = np.linalg.norm(g_current[:3])
 
                             K_current = self.computeKContractility(l_i0, y_1, y_2, C)
-                            kg_functions.assembleK(K_current, cell.globalIds[currentTri.Edge])
+                            self.K = kg_functions.assembleK(self.K, K_current, cell.globalIds[currentTri.Edge])
 
                             Energy_c += self.computeEnergyContractility(l_i0, np.linalg.norm(y_1 - y_2), C)
                 self.g += ge
