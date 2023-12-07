8eccb76f01ece1d6b532d59d708ec699754312a2
Pablo Vicente Munuera <pablovm1990@gmail.com>
2023-12-07 11:27:01 +0000

    triAR contribution as matlab now

--- src/pyVertexModel/Kg/kgTriAREnergyBarrier.py
+++ src/pyVertexModel/Kg/kgTriAREnergyBarrier.py
@@ -10,7 +10,7 @@ class KgTriAREnergyBarrier(Kg):
         start = time.time()
 
         self.energy = 0
-        fact = Set.lambdaR / Set.lmin0 ^ 4
+        fact = Set.lambdaR / Set.lmin0 ** 4
 
         for c in [cell.ID for cell in Geo.Cells if cell.AliveStatus == 1]:
             # if Geo.Remodelling and c not in Geo.AssembleNodes:
@@ -37,36 +37,37 @@ class KgTriAREnergyBarrier(Kg):
                         y2 = Ys[Tris[t].Edge[1], :]
                         y3 = Cell.Faces[f].Centre
 
-                        ys = np.zeros([3, 3, 3], dtype=np.float32)
+                        ys = np.zeros([3, 3, 3], dtype=self.precision_type)
                         nY = np.zeros([3, 3], dtype=int)
 
                         y12 = y1 - y2
                         y23 = y2 - y3
                         y31 = y3 - y1
 
-                        w1 = np.linalg.norm(y31) ^ 2 - np.linalg.norm(y12) ^ 2
-                        w2 = np.linalg.norm(y12) ^ 2 - np.linalg.norm(y23) ^ 2
-                        w3 = np.linalg.norm(y23) ^ 2 - np.linalg.norm(y31) ^ 2
+                        w1 = np.linalg.norm(y31) ** 2 - np.linalg.norm(y12) ** 2
+                        w2 = np.linalg.norm(y12) ** 2 - np.linalg.norm(y23) ** 2
+                        w3 = np.linalg.norm(y23) ** 2 - np.linalg.norm(y31) ** 2
 
-                        g1 = np.array([y23, y12, y31], dtype=np.float32)
-                        g2 = np.array([y12, y31, y23], dtype=np.float32)
-                        g3 = np.array([y31, y23, y12], dtype=np.float32)
+                        g1 = np.array([y23, y12, y31]).reshape(-1, 1)
+                        g2 = np.array([y12, y31, y23]).reshape(-1, 1)
+                        g3 = np.array([y31, y23, y12]).reshape(-1, 1)
 
-                        gs = 2 * (w1 * g1 + w2 * g2 + w3 * g3)
+                        gs = 2 * (np.dot(w1, g1) + np.dot(w2, g2) + np.dot(w3, g3))
                         gs_fact = gs * fact
-                        self.g = self.assemble_g(self.g[:], gs_fact[:], nY_original)
+                        self.g = self.assemble_g(self.g[:], gs_fact.flatten(), nY_original)
 
                         if calculate_K:
                             identity = np.eye(3, 3)
-                            Ks = 2 * np.block([(w2 - w3) * identity, (w1 - w2) * identity, (w3 - w1) * identity,
-                                              (w1 - w2) * identity, (w3 - w1) * identity, (w2 - w3) * identity,
-                                              (w3 - w1) * identity, (w2 - w3) * identity, (w1 - w2) * identity]) + 4
+                            Ks = 2 * np.block([[(w2 - w3) * identity, (w1 - w2) * identity, (w3 - w1) * identity],
+                                               [(w1 - w2) * identity, (w3 - w1) * identity, (w2 - w3) * identity],
+                                               [(w3 - w1) * identity, (w2 - w3) * identity, (w1 - w2) * identity]])
 
-                            Ks_c = np.array(Ks * (np.dot(g1, g1.T) + np.dot(g2, g2.T) + np.dot(g3, g3.T)) * fact,
-                                            dtype=np.float32)
+                            Ks_c = Ks + 4 * (np.dot(g1, g1.T) +
+                                         np.dot(g2, g2.T) +
+                                         np.dot(g3, g3.T)) * fact
                             self.assemble_k(Ks_c[:, :], nY_original)
 
-                        energy_c = energy_c + fact / 2 * (w1 ^ 2 + w2 ^ 2 + w3 ^ 2)
+                        energy_c = energy_c + fact / 2 * (w1 ** 2 + w2 ** 2 + w3 ** 2)
 
             self.energy += energy_c
 
