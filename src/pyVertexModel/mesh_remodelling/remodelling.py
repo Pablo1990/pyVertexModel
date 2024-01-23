ea8cd797c0b54d85e5e50fe87c378a3a2354fcfd
Pablo Vicente Munuera <pablovm1990@gmail.com>
2024-01-23 09:05:00 +0000

    methods to function

--- src/pyVertexModel/mesh_remodelling/remodelling.py
+++ src/pyVertexModel/mesh_remodelling/remodelling.py
@@ -28,6 +28,119 @@ def get_faces_from_node(geo, nodes):
     return faces, faces_tris
 
 
+def add_edge_to_intercalate(geo, num_cell, segment_features, edge_lengths_top, edges_to_intercalate_top,
+                            ghost_node_id):
+    """
+    Add an edge to intercalate.
+    :param geo:
+    :param num_cell:
+    :param segment_features:
+    :param edge_lengths_top:
+    :param edges_to_intercalate_top:
+    :param ghost_node_id:
+    :return:
+    """
+    for neighbour_to_num_cell in np.where(edges_to_intercalate_top)[0]:
+        neighbours_1 = get_node_neighbours_per_domain(geo, num_cell, ghost_node_id)
+        neighbours_2 = get_node_neighbours_per_domain(geo, neighbour_to_num_cell, ghost_node_id)
+        shared_neighbours = np.intersect1d(neighbours_1, neighbours_2)
+
+        shared_ghost_nodes = shared_neighbours[np.isin(shared_neighbours, geo.XgID)]
+
+        for node_pair_g in shared_ghost_nodes:
+            neighbours_2 = [get_node_neighbours_per_domain(geo, node_pair_g, node_pair_g)]
+            shared_neighbours = np.intersect1d(neighbours_1, neighbours_2)
+            shared_neighbours_c = shared_neighbours[~np.isin(shared_neighbours, geo.XgID)]
+            shared_neighbours_c = shared_neighbours_c[shared_neighbours_c != neighbour_to_num_cell]
+
+            if any([geo.Cells[neighbour].AliveStatus == 1 for neighbour in shared_neighbours_c]):
+                cell_to_intercalate = [neighbour for neighbour in shared_neighbours_c if
+                                       geo.Cells[neighbour].AliveStatus == 1]
+            else:
+                continue
+
+            c_face, _ = get_faces_from_node(geo, [num_cell, node_pair_g])
+            num_shared_neighbours = len(shared_neighbours)
+            face_global_id = c_face[0].globalIds
+            cell_to_split_from = neighbour_to_num_cell
+
+            for cell_intercalate in cell_to_intercalate:
+                new_row = {
+                    'num_cell': num_cell,
+                    'node_pair_g': node_pair_g,
+                    'cell_intercalate': cell_intercalate,
+                    'cell_to_split_from': cell_to_split_from,
+                    'edge_length': edge_lengths_top[neighbour_to_num_cell],
+                    'num_shared_neighbours': num_shared_neighbours,
+                    'shared_neighbours': [shared_neighbours],
+                    'face_global_id': face_global_id,
+                    'neighbours_1': [neighbours_1],
+                    'neighbours_2': neighbours_2
+                }
+                segment_features = pd.concat([segment_features, pd.DataFrame([new_row])], ignore_index=True)
+
+
+def solve_remodeling_step(geo_0, geo_n, geo, dofs, set):
+    """
+    This function solves local problem to obtain the position of the newly remodeled vertices with prescribed settings
+    (Set.***_LP), e.g. Set.lambda_LP.
+    :param geo_0:
+    :param geo_n:
+    :param geo:
+    :param dofs:
+    :param set:
+    :return:
+    """
+
+    geop = geo.copy()  # Assuming there is a method to copy the Geo object
+    logger.info('=====>> Solving Local Problem....')
+    geo.remodel_mesh = True
+    increase_eta = True
+    original_nu = set.nu
+
+    set.nu0 = set.nu
+    set.nu = set.nu_lp_initial
+    set.max_iter = set.max_iter0 * 3
+    did_not_converge = False
+
+    while True:
+        g, k, _ = KgGlobal(geo_0, geo_n, geo, set)
+
+        dy = np.zeros((geo.numF + geo.numY + geo.n_cells) * 3)
+        dyr = np.linalg.norm(dy[dofs.remodel])
+        gr = np.linalg.norm(g[dofs.remodel])
+        logger.info(
+            f'Local Problem ->Iter: 0, ||gr||= {gr:.3e} ||dyr||= {dyr:.3e}  nu/nu0={set.nu / set.nu0:.3e}  dt/dt0={set.dt / set.dt0:.3g}')
+
+        geo, g, k, energy, set, gr, dyr, dy = newton_raphson(geo_0, geo_n, geo, dofs, set, k, g, -1, -1)
+
+        if increase_eta and (gr > set.tol or dyr > set.tol):
+            geo = geop.copy()
+            logger.info("Convergence was not achieved ...")
+            logger.info('First strategy ---> Restart iterating while higher viscosity...')
+            set.nu *= 10
+            set.max_iter = set.max_iter0 * 4
+            increase_eta = False
+        elif gr > set.tol or dyr > set.tol or np.any(np.isnan(g[dofs.free])) or np.any(np.isnan(dy[dofs.free])):
+            logger.info(f'Local Problem did not converge after {set.iter} iterations.')
+            did_not_converge = True
+            set.max_iter = set.max_iter0
+            set.nu = original_nu
+            break
+        else:
+            if set.nu / set.nu0 == 1:
+                logger.info(f'=====>> Local Problem converged in {set.iter} iterations.')
+                did_not_converge = False
+                set.max_iter = set.max_iter0
+                set.nu = original_nu
+                geo.remodel_mesh = False
+                break
+            else:
+                set.nu = max(set.nu / 2, set.nu0)
+
+    return geo, set, did_not_converge
+
+
 class Remodelling:
     """
     Class that contains the information of the remodelling process.
@@ -120,7 +233,7 @@ class Remodelling:
 
                 Dofs = DegreesOfFreedom.get_dofs(Geo, self.Set)
                 Geo = Dofs.get_remodel_dofs(allTnew, Geo)
-                Geo, Set, DidNotConverge = self.solve_remodeling_step(Geo_0, Geo_n, Geo, Dofs, Set)
+                Geo, Set, DidNotConverge = solve_remodeling_step(Geo_0, Geo_n, Geo, Dofs, Set)
                 if DidNotConverge:
                     Geo = Geo_backup
                     Geo_n = Geo_n_backup
@@ -152,115 +265,10 @@ class Remodelling:
                         rowsToRemove.append(numRow)
             segmentFeatures_all = [feature for i, feature in enumerate(segmentFeatures_all) if i not in rowsToRemove]
 
-    def add_edge_to_intercalate(self, geo, num_cell, segment_features, edge_lengths_top, edges_to_intercalate_top,
-                                ghost_node_id):
-        """
-        Add an edge to intercalate.
-        :param geo:
-        :param num_cell:
-        :param segment_features:
-        :param edge_lengths_top:
-        :param edges_to_intercalate_top:
-        :param ghost_node_id:
-        :return:
-        """
-        for neighbour_to_num_cell in np.where(edges_to_intercalate_top)[0]:
-            neighbours_1 = get_node_neighbours_per_domain(geo, num_cell, ghost_node_id)
-            neighbours_2 = get_node_neighbours_per_domain(geo, neighbour_to_num_cell, ghost_node_id)
-            shared_neighbours = np.intersect1d(neighbours_1, neighbours_2)
-
-            shared_ghost_nodes = shared_neighbours[np.isin(shared_neighbours, geo.XgID)]
-
-            for node_pair_g in shared_ghost_nodes:
-                neighbours_2 = [get_node_neighbours_per_domain(geo, node_pair_g, node_pair_g)]
-                shared_neighbours = np.intersect1d(neighbours_1, neighbours_2)
-                shared_neighbours_c = shared_neighbours[~np.isin(shared_neighbours, geo.XgID)]
-                shared_neighbours_c = shared_neighbours_c[shared_neighbours_c != neighbour_to_num_cell]
-
-                if any([geo.Cells[neighbour].AliveStatus == 1 for neighbour in shared_neighbours_c]):
-                    cell_to_intercalate = [neighbour for neighbour in shared_neighbours_c if
-                                           geo.Cells[neighbour].AliveStatus == 1]
-                else:
-                    continue
-
-                c_face, _ = get_faces_from_node(geo, [num_cell, node_pair_g])
-                num_shared_neighbours = len(shared_neighbours)
-                face_global_id = c_face[0].globalIds
-                cell_to_split_from = neighbour_to_num_cell
-
-                for cell_intercalate in cell_to_intercalate:
-                    new_row = {
-                        'num_cell': num_cell,
-                        'node_pair_g': node_pair_g,
-                        'cell_intercalate': cell_intercalate,
-                        'cell_to_split_from': cell_to_split_from,
-                        'edge_length': edge_lengths_top[neighbour_to_num_cell],
-                        'num_shared_neighbours': num_shared_neighbours,
-                        'shared_neighbours': [shared_neighbours],
-                        'face_global_id': face_global_id,
-                        'neighbours_1': [neighbours_1],
-                        'neighbours_2': neighbours_2
-                    }
-                    segment_features = pd.concat([segment_features, pd.DataFrame([new_row])], ignore_index=True)
-
-    def solve_remodeling_step(self, geo_0, geo_n, geo, dofs, set):
-        """
-        This function solves local problem to obtain the position of the newly
-        remodeled vertices with prescribed settings (Set.***_LP), e.g.
-        Set.lambda_LP.
-        """
-        geop = geo.copy()  # Assuming there is a method to copy the Geo object
-        logger.info('=====>> Solving Local Problem....')
-        geo.remodel_mesh = True
-        increase_eta = True
-        original_nu = set.nu
-
-        set.nu0 = set.nu
-        set.nu = set.nu_lp_initial
-        set.max_iter = set.max_iter0 * 3
-        did_not_converge = False
-
-        while True:
-            g, k, _ = KgGlobal(geo_0, geo_n, geo, set)
-
-            dy = np.zeros((geo.numF + geo.numY + geo.n_cells) * 3)
-            dyr = np.linalg.norm(dy[dofs.remodel])
-            gr = np.linalg.norm(g[dofs.remodel])
-            logger.info(
-                f'Local Problem ->Iter: 0, ||gr||= {gr:.3e} ||dyr||= {dyr:.3e}  nu/nu0={set.nu / set.nu0:.3e}  dt/dt0={set.dt / set.dt0:.3g}')
-
-            geo, g, k, energy, set, gr, dyr, dy = newton_raphson(geo_0, geo_n, geo, dofs, set, k, g, -1, -1)
-
-            if increase_eta and (gr > set.tol or dyr > set.tol):
-                geo = geop.copy()
-                logger.info("Convergence was not achieved ...")
-                logger.info('First strategy ---> Restart iterating while higher viscosity...')
-                set.nu *= 10
-                set.max_iter = set.max_iter0 * 4
-                increase_eta = False
-            elif gr > set.tol or dyr > set.tol or np.any(np.isnan(g[dofs.free])) or np.any(np.isnan(dy[dofs.free])):
-                logger.info(f'Local Problem did not converge after {set.iter} iterations.')
-                did_not_converge = True
-                set.max_iter = set.max_iter0
-                set.nu = original_nu
-                break
-            else:
-                if set.nu / set.nu0 == 1:
-                    logger.info(f'=====>> Local Problem converged in {set.iter} iterations.')
-                    did_not_converge = False
-                    set.max_iter = set.max_iter0
-                    set.nu = original_nu
-                    geo.remodel_mesh = False
-                    break
-                else:
-                    set.nu = max(set.nu / 2, set.nu0)
-
-        return geo, set, did_not_converge
-
     def get_tris_to_remodel_ordered(self):
         """
         Obtain the edges that are going to be remodeled.
-        :return:
+        :return: segment_features_filtered (list): List of edges to remodel.
         """
         segment_features = pd.DataFrame()
         for num_cell in self.Geo.non_dead_cells:
@@ -301,9 +309,17 @@ class Remodelling:
         return segment_features_filtered
 
     def check_edges_to_intercalate(self, edge_lengths, num_cell, segment_features, ghost_node_id):
+        """
+        Check the edges to intercalate.
+        :param edge_lengths:
+        :param num_cell:
+        :param segment_features:
+        :param ghost_node_id:
+        :return:
+        """
         if np.any(edge_lengths > 0):
             avg_edge_length = np.median(edge_lengths[edge_lengths > 0])
             edges_to_intercalate = (edge_lengths < avg_edge_length - (
                     self.Set.RemodelStiffness * avg_edge_length)) & (edge_lengths > 0)
-            self.add_edge_to_intercalate(self.Geo, num_cell, segment_features, edge_lengths, edges_to_intercalate,
-                                         ghost_node_id)
+            add_edge_to_intercalate(self.Geo, num_cell, segment_features, edge_lengths, edges_to_intercalate,
+                                    ghost_node_id)
