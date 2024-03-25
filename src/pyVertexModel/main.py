bde9182e4151b2d66d1883c4288f932ddcc98a94
Pablo Vicente Munuera <pablovm1990@gmail.com>
2024-03-25 09:20:18 +0000

    minor

--- src/pyVertexModel/main.py
+++ src/pyVertexModel/main.py
@@ -4,7 +4,6 @@ from src.pyVertexModel.util.utils import load_state
 vModel = VertexModelVoronoiFromTimeImage()
 vModel.initialize('/Users/pablovm/PostDoc/pyVertexModel/Tests/data/Newton_Raphson_Iteration_wingdisc.mat')
 vModel.iterate_over_time()
-#load_state(vModel, '/Users/pablovm/PostDoc/pyVertexModel/Result/02-21_143554_VertexModelTime_Cells_40_visc_5000_lVol_10_muBulk_0_lBulk_0_kSubs_100_lt_0.05_noise_0.1_eTriAreaBarrier_1_eARBarrier_0.3_RemStiff_0_lS1_10_lS2_1.0_lS3_1.0/data_step_4.pkl')
-#vModel.tr = -1
-#vModel.set.RemodelStiffness = 0.7
+#load_state(vModel, '/Users/pablovm/PostDoc/pyVertexModel/Result/03-21_085603_VertexModelTime_Cells_40_visc_5000_lVol_10_muBulk_0_lBulk_0_kSubs_100_lt_0.05_noise_0.0_eTriAreaBarrier_1_eARBarrier_0.3_RemStiff_0.7_lS1_10_lS2_1.0_lS3_1.0/data_step_before_remodelling_3.pkl')
 #vModel.iteration_converged()
+#vModel.iterate_over_time()
