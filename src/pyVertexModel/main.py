8060de923762b03b823356e7836cf12d7a7176eb
Pablo Vicente Munuera <pablovm1990@gmail.com>
2024-06-03 10:43:05 +0100

    adding to main

--- src/pyVertexModel/main.py
+++ src/pyVertexModel/main.py
@@ -15,3 +15,5 @@ else:
                '/media/pablo/d7c61090-024c-469a-930c-f5ada47fb049/PabloVicenteMunuera/VertexModel/pyVertexModel/Result/04-14_144819_VertexModelTime_Cells_110_visc_500_lVol_0.001_kSubs_1_lt_0.02_noise_0.2_brownian_0.001_eTriAreaBarrier_5_eARBarrier_0.01_RemStiff_0.7_lS1_5_lS2_0.5_lS3_0.5/data_step_before_remodelling_30.pkl')
     vModel.iteration_converged()
     vModel.iterate_over_time()
+
+analyse_simulation(vModel.set.OutputFolder)
