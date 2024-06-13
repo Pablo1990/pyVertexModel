7c1be8b283825a27f52e3b00577b7ee8f8f338e2
Pablo Vicente Munuera <pablovm1990@gmail.com>
2024-06-13 12:21:05 +0100

    create vtk files, just in case

--- src/pyVertexModel/analysis/obtain_vtks_from_file.py
+++ src/pyVertexModel/analysis/obtain_vtks_from_file.py
@@ -3,10 +3,22 @@ import os
 from src.pyVertexModel.algorithm.vertexModel import VertexModel
 from src.pyVertexModel.util.utils import load_state
 
-folder = '/media/pablo/d7c61090-024c-469a-930c-f5ada47fb049/PabloVicenteMunuera/VertexModel/Results/Relevant/'
-folder_2 = '/media/pablo/d7c61090-024c-469a-930c-f5ada47fb049/PabloVicenteMunuera/VertexModel/pyVertexModel/Result/'
-files = '05-30_132736_VertexModelTime_Cells_150_visc_100_lVol_0.01_kSubs_1_lt_0.0025_noise_0_brownian_0_eTriAreaBarrier_0_eARBarrier_0.001_RemStiff_0.98_lS1_0.01_lS2_0.01_lS3_0.01_pString_2.5_tol_5.0'
-vtk_dir = folder + files
-vtk_dir_output = folder_2 + files
+folder = '/media/pablo/d7c61090-024c-469a-930c-f5ada47fb049/PabloVicenteMunuera/VertexModel/Results/Relevant/ToUse'
+lst = os.listdir(folder)
+lst.sort(reverse=True)
 
+for c_dir in lst:
+    print(c_dir)
+    # if file is a directory
+    if os.path.isdir(os.path.join(folder, c_dir)):
+        vModel = VertexModel(create_output_folder=False)
+        if os.path.exists(os.path.join(folder, c_dir, 'Cells')):
+            continue
 
+        for file_id, file in enumerate(os.listdir(os.path.join(folder, c_dir))):
+            if file.endswith('.pkl') and not file.__contains__('data_step_before_remodelling'):
+                # Load the state of the model
+                load_state(vModel, os.path.join(folder, c_dir, file))
+                vModel.set.VTK = True
+                vModel.set.OutputFolder = os.path.join(folder, c_dir)
+                vModel.geo.create_vtk_cell(vModel.set, vModel.numStep, 'Cells')
