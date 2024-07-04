8c9741a76d72b4b0125ba87bce83f026aa3c720b
Pablo Vicente Munuera <pablovm1990@gmail.com>
2024-07-04 15:33:40 +0100

    main generic of any number of cells

--- src/pyVertexModel/main.py
+++ src/pyVertexModel/main.py
@@ -8,12 +8,16 @@ vModel = VertexModelVoronoiFromTimeImage()
 
 start_new = True
 if start_new == True:
-    vModel.initialize(
-        '/media/pablo/d7c61090-024c-469a-930c-f5ada47fb049/PabloVicenteMunuera/VertexModel/pyVertexModel/Tests/data/wing_disc_150.mat')
+    #vModel.initialize(
+    #    '/media/pablo/d7c61090-024c-469a-930c-f5ada47fb049/PabloVicenteMunuera/VertexModel/pyVertexModel/Tests/data/wing_disc_150.mat')
+    vModel.initialize()
+    temp_dir = os.path.join(vModel.set.OutputFolder, 'images')
+    if not os.path.exists(temp_dir):
+        os.makedirs(temp_dir)
     vModel.iterate_over_time()
 else:
     load_state(vModel,
-               '/media/pablo/d7c61090-024c-469a-930c-f5ada47fb049/PabloVicenteMunuera/VertexModel/pyVertexModel/Result/04-14_144819_VertexModelTime_Cells_110_visc_500_lVol_0.001_kSubs_1_lt_0.02_noise_0.2_brownian_0.001_eTriAreaBarrier_5_eARBarrier_0.01_RemStiff_0.7_lS1_5_lS2_0.5_lS3_0.5/data_step_before_remodelling_30.pkl')
+               '/media/pablo/d7c61090-024c-469a-930c-f5ada47fb049/PabloVicenteMunuera/VertexModel/pyVertexModel/Result/07-04_113437_VertexModelTime_Cells_150_visc_100_lVol_1_kSubs_0_lt_0.0001_noise_0_brownian_0_eTriAreaBarrier_20_eARBarrier_0_RemStiff_0.95_lS1_0.1_lS2_0.1_lS3_0.1_pString_5/data_step_before_remodelling_278.pkl')
     vModel.iteration_converged()
     vModel.iterate_over_time()
 
