ff6c0d64504c934368228cbc0b4e22f54e01463f
Pablo Vicente Munuera <pablovm1990@gmail.com>
2024-07-12 15:15:38 +0100

    init vertexModel_voronoi

---
+++ src/pyVertexModel/algorithm/vertexModel_Voronoi3D.py
@@ -0,0 +1,17 @@
+from src.pyVertexModel.algorithm.vertexModel import VertexModel
+
+
+class vertexModel_Voronoi3D(VertexModel):
+    """
+    Vertex model for 3D voronoi tessellation.
+    """
+    
+    def __init__(self, set_test=None):
+        super().__init__(set_test)
+
+    def initialize(self):
+        """
+        Initialize the vertex model.
+        """
+        pass
+
