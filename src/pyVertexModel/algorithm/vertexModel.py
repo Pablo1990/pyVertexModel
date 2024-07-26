7a27935c657992e80a9083b3898f8a34ebbd4b6a
Pablo Vicente Munuera <pablovm1990@gmail.com>
2024-07-26 10:11:58 +0100

    should work same cmap after ablation

--- src/pyVertexModel/algorithm/vertexModel.py
+++ src/pyVertexModel/algorithm/vertexModel.py
@@ -152,7 +152,7 @@ class VertexModel:
         :param c_set:
         """
 
-        self.color_mapping = None
+        self.colormap_lim = None
         self.OutputFolder = None
         self.numStep = None
         self.backupVars = None
@@ -530,13 +530,9 @@ class VertexModel:
         """
         total_real_cells = len([cell.ID for cell in self.geo.Cells if cell.AliveStatus is not None])
 
-        # Create a colormap
-        if self.color_mapping is None:
-            new_colours = True
-            colormap = plt.get_cmap('prism', total_real_cells)
-            self.color_mapping = {}
-        else:
-            new_colours = False
+        # Create a colormap_lim
+        if self.colormap_lim is None:
+            self.colormap_lim = [0, total_real_cells]
 
         # Create a plotter
         if selected_cells is None:
@@ -547,13 +543,10 @@ class VertexModel:
             if cell.AliveStatus == 1 and (cell.ID in selected_cells or selected_cells is not []):
                 # Load the VTK file as a pyvista mesh
                 mesh = cell.create_pyvista_mesh()
-                if new_colours:
-                    # Generate a new color from the colormap and store it in the dictionary
-                    self.color_mapping[cell.ID] = colormap(cell.ID / total_real_cells)[:3]
 
                 # Add the mesh to the plotter
-                plotter.add_mesh(mesh, scalars='ID', lighting=True, color=self.color_mapping[cell.ID], show_edges=True, edge_opacity=0.5,
-                                 edge_color='grey')
+                plotter.add_mesh(mesh, scalars='ID', lighting=True, cmap="prism", clim=self.colormap_lim,
+                                 show_edges=True, edge_opacity=0.5, edge_color='grey')
         # Set a fixed camera zoom level
         fixed_zoom_level = 1
         plotter.camera.zoom(fixed_zoom_level)
