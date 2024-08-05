32ef38b5fd249eb33333e32967a10e1fd79c2abe
Pablo Vicente Munuera <pablovm1990@gmail.com>
2024-08-05 10:26:59 +0100

    updating main

--- src/pyVertexModel/main.py
+++ src/pyVertexModel/main.py
@@ -9,14 +9,14 @@ vModel = VertexModelVoronoiFromTimeImage()
 start_new = True
 if start_new == True:
     vModel.initialize()
-    temp_dir = os.path.join(vModel.set.OutputFolder, 'images')
-    if not os.path.exists(temp_dir):
-        os.makedirs(temp_dir)
     vModel.iterate_over_time()
 else:
     load_state(vModel,
-               '/media/pablo/d7c61090-024c-469a-930c-f5ada47fb049/PabloVicenteMunuera/VertexModel/pyVertexModel/Result/07-04_113437_VertexModelTime_Cells_150_visc_100_lVol_1_kSubs_0_lt_0.0001_noise_0_brownian_0_eTriAreaBarrier_20_eARBarrier_0_RemStiff_0.95_lS1_0.1_lS2_0.1_lS3_0.1_pString_5/data_step_before_remodelling_278.pkl')
-    vModel.iteration_converged()
+               '/media/pablo/d7c61090-024c-469a-930c-f5ada47fb049/PabloVicenteMunuera/VertexModel/pyVertexModel/Result/'
+               '08-01_164939__Cells_150_visc_100_lVol_1_kSubs_0.01_lt_0.0015_ltExt_0.00015_noise_0_ref_A0_1_eTriAreaBarrier_0_eARBarrier_0_RemStiff_0.95_lS1_4_lS2_0.4_lS3_0.4_pString_0/'
+               'data_step_300.pkl')
+    vModel.set.wing_disc()
+    vModel.set.wound_default()
     vModel.iterate_over_time()
 
 analyse_simulation(vModel.set.OutputFolder)
