a84bcbd0685394ae39b93ccf3d5c0ce54e0a2ed9
Pablo Vicente Munuera <pablovm1990@gmail.com>
2024-08-07 09:57:48 +0100

    external l_0 same to all domains

--- src/pyVertexModel/Kg/kgContractility_external.py
+++ src/pyVertexModel/Kg/kgContractility_external.py
@@ -33,49 +33,48 @@ class KgContractilityExternal(KgContractility):
                 cell.contractility_noise = random.random()
 
             for face_id, currentFace in enumerate(cell.Faces):
-                l_i0 = geo.EdgeLengthAvg_0[next(key for key, value in currentFace.InterfaceType_allValues.items()
-                                                if
-                                                value == currentFace.InterfaceType or key == currentFace.InterfaceType)]
+                l_i0 = np.mean(geo.EdgeLengthAvg_0)
                 # if currentFace.InterfaceType == 1 or currentFace.InterfaceType == 'Lateral':
                 #     continue
 
+                # Same as contractility for edges
+                if currentFace.InterfaceType == 1 or currentFace.InterfaceType == 'Lateral':
+                    C = c_set.cLineTension_external / 10
+                elif currentFace.InterfaceType == 0 or currentFace.InterfaceType == 'Top':
+                    C = c_set.cLineTension_external
+                elif currentFace.InterfaceType == 2 or currentFace.InterfaceType == 'Bottom':
+                    C = c_set.cLineTension_external / 10
+
+                #TODO: I HAVE CHANGED THIS BECAUSE LATERAL VERTICES WERE MOVING TOO MUCH AWAY FROM THE BORDER
+
                 for tri_id, currentTri in enumerate(currentFace.Tris):
-                    tets_1 = cell.T[currentTri.Edge[0]]
-                    tets_2 = cell.T[currentTri.Edge[1]]
-                    if np.any(np.isin(geo.BorderGhostNodes, tets_1)) or np.any(np.isin(geo.BorderGhostNodes, tets_2)):
-                        C = c_set.cLineTension_external
-                        if np.any(np.isin(geo.BorderGhostNodes, tets_2)):
-                            edge_id = currentTri.Edge[1]
-                            c_tets = tets_2
-                        elif np.any(np.isin(geo.BorderGhostNodes, tets_1)):
-                            edge_id = currentTri.Edge[0]
-                            c_tets = tets_1
-                        else:
-                            continue
+                    for edge_id in currentTri.Edge:
+                        c_tets = cell.T[edge_id]
 
-                        y_1 = cell.Y[edge_id]
-                        borderNode = geo.BorderGhostNodes[np.isin(geo.BorderGhostNodes, c_tets)][0]
-                        y_2 = [cell.X for cell in geo.Cells if cell.ID == borderNode][0]
+                        if np.any(np.isin(geo.BorderGhostNodes, c_tets)):
+                            y_1 = cell.Y[edge_id]
+                            borderNode = geo.BorderGhostNodes[np.isin(geo.BorderGhostNodes, c_tets)][0]
+                            y_2 = [cell.X for cell in geo.Cells if cell.ID == borderNode][0]
 
-                        if currentFace.InterfaceType == 0 or currentFace.InterfaceType == 'Top':
-                            z_position = [cell.X for cell in geo.Cells if cell.ID == geo.XgTop[0]][0][2]
-                            y_2[2] = z_position / 2
-                        elif currentFace.InterfaceType == 2 or currentFace.InterfaceType == 'Bottom':
-                            z_position = [cell.X for cell in geo.Cells if cell.ID == geo.XgBottom[0]][0][2]
-                            y_2[2] = z_position / 2
+                            if currentFace.InterfaceType == 0 or currentFace.InterfaceType == 'Top':
+                                z_position = [cell.X for cell in geo.Cells if cell.ID == geo.XgTop[0]][0][2]
+                                y_2[2] = z_position / 2
+                            elif currentFace.InterfaceType == 2 or currentFace.InterfaceType == 'Bottom':
+                                z_position = [cell.X for cell in geo.Cells if cell.ID == geo.XgBottom[0]][0][2]
+                                y_2[2] = z_position / 2
 
-                        if geo.remodelling and not np.any(np.isin(cell.globalIds[edge_id],
-                                                                  geo.Cells[c].vertices_and_faces_to_remodel)):
-                            continue
+                            if geo.remodelling and not np.any(np.isin(cell.globalIds[edge_id],
+                                                                      geo.Cells[c].vertices_and_faces_to_remodel)):
+                                continue
 
-                        g_current = self.compute_g_contractility(l_i0, y_1, y_2, C)
-                        ge = self.assemble_g(ge[:], g_current[:3], [cell.globalIds[edge_id]])
+                            g_current = self.compute_g_contractility(l_i0, y_1, y_2, C)
+                            ge = self.assemble_g(ge[:], g_current[:3], [cell.globalIds[edge_id]])
 
-                        if calculate_k:
-                            K_current = self.compute_k_contractility(l_i0, y_1, y_2, C)
-                            self.assemble_k(K_current[:, :], [cell.globalIds[edge_id]])
+                            if calculate_k:
+                                K_current = self.compute_k_contractility(l_i0, y_1, y_2, C)
+                                self.assemble_k(K_current[:, :], [cell.globalIds[edge_id]])
 
-                        Energy_c += compute_energy_contractility(l_i0, np.linalg.norm(y_1 - y_2), C)
+                            Energy_c += compute_energy_contractility(l_i0, np.linalg.norm(y_1 - y_2), C)
             self.g += ge
             Energy[c] = Energy_c
             cell.contractility_noise = None
