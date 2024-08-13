844eed0e218fb4828695025e724a7ea3c8f54626
Pablo Vicente Munuera <pablovm1990@gmail.com>
2024-08-13 15:19:44 +0100

    quantifying smaller cells than average over time

--- src/pyVertexModel/analysis/analyse_simulations.py
+++ src/pyVertexModel/analysis/analyse_simulations.py
@@ -17,17 +17,21 @@ for _, file in enumerate(lst):
     if os.path.isdir(os.path.join(folder, file)):
         files_within_folder = os.listdir(os.path.join(folder, file))
         if len(files_within_folder) > 300:
+            # Analyse the simulation
+            features_per_time_df, post_wound_features, important_features, features_per_time_all_cells_df = (
+                analyse_simulation(os.path.join(folder, file)))
+
             # Analyse the edge recoil
             if os.path.exists(os.path.join(folder, file, 'data_step_300.pkl')):
                 # if the analysis file exists, load it
-                if os.path.exists(os.path.join(folder, file, 'recoil_info_basal.pkl')):
-                    recoiling_info_df_basal = load_variables(
-                        os.path.join(folder, file, 'recoil_info_basal.pkl'))['recoiling_info_df_basal']
+                if os.path.exists(os.path.join(folder, file, 'recoil_info_apical.pkl')):
+                    # recoiling_info_df_basal = load_variables(
+                    #     os.path.join(folder, file, 'recoil_info_basal.pkl'))['recoiling_info_df_basal']
                     recoiling_info_df_apical = load_variables(
                         os.path.join(folder, file, 'recoil_info_apical.pkl'))['recoiling_info_df_apical']
                 else:
                     n_ablations = 1
-                    t_end = 100
+                    t_end = 5
                     recoiling_info = analyse_edge_recoil(os.path.join(folder, file, 'data_step_300.pkl'),
                                                          n_ablations=n_ablations, location_filter=0, t_end=t_end)
                     recoiling_info_df_apical = pd.DataFrame(recoiling_info)
@@ -35,23 +39,30 @@ for _, file in enumerate(lst):
                     save_variables({'recoiling_info_df_apical': recoiling_info_df_apical},
                                    os.path.join(folder, file, 'recoil_info_apical.pkl'))
 
-                    recoiling_info = analyse_edge_recoil(os.path.join(folder, file, 'data_step_300.pkl'),
-                                                         n_ablations=n_ablations, location_filter=2, t_end=t_end)
-                    recoiling_info_df_basal = pd.DataFrame(recoiling_info)
-                    recoiling_info_df_basal.to_excel(os.path.join(folder, file, 'recoil_info_basal.xlsx'))
-                    save_variables({'recoiling_info_df_basal': recoiling_info_df_basal},
-                                   os.path.join(folder, file, 'recoil_info_basal.pkl'))
-
-            # Analyse the simulation
-            features_per_time_df, post_wound_features, important_features, features_per_time_all_cells_df = (
-                analyse_simulation(os.path.join(folder, file)))
+                    # recoiling_info = analyse_edge_recoil(os.path.join(folder, file, 'data_step_300.pkl'),
+                    #                                      n_ablations=n_ablations, location_filter=2, t_end=t_end)
+                    # recoiling_info_df_basal = pd.DataFrame(recoiling_info)
+                    # recoiling_info_df_basal.to_excel(os.path.join(folder, file, 'recoil_info_basal.xlsx'))
+                    # save_variables({'recoiling_info_df_basal': recoiling_info_df_basal},
+                    #                os.path.join(folder, file, 'recoil_info_basal.pkl'))
 
             if important_features is not None and len(important_features) > 5:
-                important_features['recoiling_speed_apical'] = np.mean(recoiling_info_df_apical['recoil_speed'])
-                important_features['recoiling_speed_apical_std'] = np.std(recoiling_info_df_apical['recoil_speed'])
+                important_features['recoiling_speed_apical'] = np.mean(recoiling_info_df_apical['initial_recoil_in_s'])
+                important_features['recoiling_speed_apical_std'] = np.std(recoiling_info_df_apical['initial_recoil_in_s'])
 
-                important_features['recoiling_speed_basal'] = np.mean(recoiling_info_df_basal['recoil_speed'])
-                important_features['recoiling_speed_basal_std'] = np.std(recoiling_info_df_basal['recoil_speed'])
+                # important_features['recoiling_speed_basal'] = np.mean(recoiling_info_df_basal['initial_recoil_in_s'])
+                # important_features['recoiling_speed_basal_std'] = np.std(recoiling_info_df_basal['initial_recoil_in_s'])
+
+                # Sort features_per_time_df by time
+                features_per_time_df_std = features_per_time_all_cells_df.groupby(by='time').std().reset_index()
+                id_before_ablation = np.where(features_per_time_df_std.time < 30)[0][-1]
+                important_features['std_difference_area_top'] = features_per_time_df_std.loc[id_before_ablation].Area_top - features_per_time_df_std.loc[0].Area_top
+
+                # Count the number of cells that have an area_top lower than the average area_top
+                features_per_time_df_mean = features_per_time_all_cells_df.groupby(by='time').mean().reset_index()
+                count_of_smalls_cells_per_time_0 = features_per_time_all_cells_df[features_per_time_all_cells_df.Area_top < features_per_time_df_mean.loc[0].Area_top].groupby(by='time').count().reset_index()
+                count_of_smalls_cells_per_time_ablation = features_per_time_all_cells_df[features_per_time_all_cells_df.Area_top < features_per_time_df_mean.loc[id_before_ablation].Area_top].groupby(by='time').count().reset_index()
+                important_features['cells_area_top_lower_than_average_diff'] = count_of_smalls_cells_per_time_ablation.loc[id_before_ablation].ID - count_of_smalls_cells_per_time_0.loc[0].ID
 
                 important_features['folder'] = file
 
