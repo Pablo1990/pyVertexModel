b66dfce7258ff2a931bf118d57d23d2889f877d6
Pablo Vicente Munuera <pablovm1990@gmail.com>
2024-08-19 12:50:12 +0100

    ad to python path

---
+++ src/pyVertexModel/util/add_to_pythonpath.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Define the project root path
+PROJECT_ROOT="/media/pablo/d7c61090-024c-469a-930c-f5ada47fb049/PabloVicenteMunuera/VertexModel/pyVertexModel"
+
+# Add the project root to the PYTHONPATH environment variable
+export PYTHONPATH="$PROJECT_ROOT:$PYTHONPATH"
