acb1144edac191faec21755e3d217633b7f68014
Pablo Vicente Munuera <pablovm1990@gmail.com>
2024-09-27 09:47:19 +0100

    imprpving in vivo analysis

--- src/pyVertexModel/analysis/analyse_in_vivo_ablation_data.py
+++ src/pyVertexModel/analysis/analyse_in_vivo_ablation_data.py
@@ -7,7 +7,7 @@ from matplotlib import pyplot as plt
 
 from src.pyVertexModel.analysis.analyse_simulation import fit_ablation_equation, recoil_model
 
-input_folder = '/media/pablo/d7c61090-024c-469a-930c-f5ada47fb049/PabloVicenteMunuera/VertexModel/pyVertexModel/data/RnG4EcadGFP_UASMbsRNAi_310317'
+input_folder = '/media/pablo/d7c61090-024c-469a-930c-f5ada47fb049/PabloVicenteMunuera/VertexModel/pyVertexModel/data/RnG4EcadGFP_UASROKRNAi_050317'
 txt_files = [f for f in os.listdir(input_folder) if f.endswith('.csv')]
 txt_files.sort()
 
@@ -24,17 +24,23 @@ for txt_file in txt_files:
     time_steps = time_info[1].values
 
     # Index time_steps_normalized with time_slices
-    time_steps_normalized_sliced = np.array([time_steps[i] for i in time_slices])
+    time_steps_normalized_sliced = np.array([time_steps[i] for i in time_slices - 1])
 
 
     time_steps_normalized = time_steps_normalized_sliced - time_steps_normalized_sliced[0]
 
-
-    K, initial_recoil = fit_ablation_equation(edge_length_final, time_steps_normalized)
-
     # Generate a plot with the edge length final and the fit for each ablation
     plt.figure()
     plt.plot(time_steps_normalized, edge_length_final, 'o')
+
+    # Keep only the first X seconds
+    time_end = 10
+    edge_length_final = edge_length_final[time_steps_normalized <= 10]
+    time_steps_normalized = time_steps_normalized[time_steps_normalized <= 10]
+
+    K, initial_recoil, error_bars = fit_ablation_equation(edge_length_final, time_steps_normalized)
+
+
     # Plot fit line of the Kelvin-Voigt model
     plt.plot(time_steps_normalized, recoil_model(time_steps_normalized, initial_recoil, K), 'r')
     plt.xlabel('Time (s)')
@@ -56,10 +62,10 @@ for txt_file in txt_files:
     file_names.append(txt_file)
 
 # Save fig_with_all_plots
-fig_with_all_plots.savefig(os.path.join(input_folder, 'all_plots.png'))
 plt.xlabel('Time (s)')
 plt.ylabel('Edge length final')
 plt.title('Ablation fit')
 plt.ylim([0, 3.25])
 plt.legend()
+fig_with_all_plots.savefig(os.path.join(input_folder, 'all_plots.png'))
 pd.DataFrame({'K': K_values, 'initial_recoil': initial_recoil_values, 'file': file_names}).to_excel(input_folder + '/ablation_fits.xlsx')
