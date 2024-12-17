b564fe095bfbc565ae4db466ed84ba8b346c6923
Pablo Vicente Munuera <pablovm1990@gmail.com>
2024-12-17 09:55:39 +0000

    missing import

--- src/pyVertexModel/mesh_remodelling/remodelling.py
+++ src/pyVertexModel/mesh_remodelling/remodelling.py
@@ -5,7 +5,7 @@ import numpy as np
 import pandas as pd
 from numpy.ma.extras import setdiff1d
 
-from src.pyVertexModel.algorithm.newtonRaphson import gGlobal
+from src.pyVertexModel.algorithm.newtonRaphson import gGlobal, newton_raphson_iteration_explicit
 from src.pyVertexModel.geometry.face import get_interface
 from src.pyVertexModel.geometry.geo import edge_valence, get_node_neighbours_per_domain, get_node_neighbours
 from src.pyVertexModel.mesh_remodelling.flip import y_flip_nm, post_flip
