17565eb0e0d881b57f5b2e0c6fab650d18afca13
Pablo Vicente Munuera <pablovm1990@gmail.com>
2024-12-18 12:01:08 +0000

    improving pipeline

--- src/pyVertexModel/main_paper_simulations.py
+++ src/pyVertexModel/main_paper_simulations.py
@@ -8,10 +8,7 @@ from src.pyVertexModel.analysis.analyse_simulation import analyse_simulation
 from src.pyVertexModel.util.utils import load_state
 
 def run_simulation(combination):
-    vModel = VertexModelVoronoiFromTimeImage()
-    vModel.initialize()
-
-    vModel = VertexModelVoronoiFromTimeImage()
+    vModel = VertexModelVoronoiFromTimeImage(create_output_folder=False)
     load_state(vModel,
                os.path.join(PROJECT_DIRECTORY, 'Result/'
                                                'new_reference/'
@@ -19,19 +16,21 @@ def run_simulation(combination):
 
     vModel.set.wing_disc()
     vModel.set.wound_default()
-    vModel.set.OutputFolder = os.path.join(PROJECT_DIRECTORY, 'Result/final_results_2/60_mins_no_{}'.format('_no_'.join(combination)))
     # Based on Tissue Fluidity paper from Rob Tetley et al. 2019
     if combination == 'Mbs':
         #“In Mbs RNAi wing discs, vertex recoil rates are increased by 40% in the tissue and decreased by 21% in the purse string compared to WT”
         vModel.set.lambdaS1 = vModel.set.lambdaS1 + (vModel.set.lambdaS1 * 0.40)
         vModel.set.purseStringStrength = vModel.set.purseStringStrength - (vModel.set.purseStringStrength * 0.21)
         vModel.set.lateralCablesStrength = vModel.set.lateralCablesStrength - (vModel.set.lateralCablesStrength * 0.21)
+        vModel.set.OutputFolder = os.path.join(PROJECT_DIRECTORY, 'Result/final_results_2/60_mins_Mbs')
     elif combination == 'Rok':
         #“In Rok RNAi wing discs, vertex recoil rates are reduced by 51% in the tissue and 35% in the purse string compared to WT”
         vModel.set.lambdaS1 = vModel.set.lambdaS1 - (vModel.set.lambdaS1 * 0.51)
         vModel.set.purseStringStrength = vModel.set.purseStringStrength - (vModel.set.purseStringStrength * 0.35)
         vModel.set.lateralCablesStrength = vModel.set.lateralCablesStrength - (vModel.set.lateralCablesStrength * 0.35)
+        vModel.set.OutputFolder = os.path.join(PROJECT_DIRECTORY, 'Result/final_results_2/60_mins_Rok')
     else:
+        vModel.set.OutputFolder = os.path.join(PROJECT_DIRECTORY, 'Result/final_results_2/60_mins_no_{}'.format('_no_'.join(combination)))
         for variable in combination:
             if variable == 'Remodelling':
                 vModel.set.Remodelling = False
@@ -41,6 +40,7 @@ def run_simulation(combination):
     vModel.set.dt0 = None
     vModel.set.dt = None
     vModel.set.update_derived_parameters()
+    vModel.set.redirect_output()
     vModel.iterate_over_time()
     analyse_simulation(vModel.set.OutputFolder)
 
@@ -53,6 +53,8 @@ for i in range(1, len(variables_to_change) + 1):
 
 combinations_of_variables.extend(['Mbs'])
 combinations_of_variables.extend(['Rok'])
+
+run_simulation(combinations_of_variables[-1])
 
 # Run simulations in parallel
 with ThreadPoolExecutor(max_workers=4) as executor:
