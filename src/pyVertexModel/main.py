19295a7201df0b3a9bf6579afeb6e705b45b2357
Pablo Vicente Munuera <pablovm1990@gmail.com>
2025-01-08 11:53:01 +0000

    updating main

--- src/pyVertexModel/main.py
+++ src/pyVertexModel/main.py
@@ -13,22 +13,22 @@ if start_new == True:
     analyse_simulation(vModel.set.OutputFolder)
 else:
     debugging = False
-    vModel = VertexModelVoronoiFromTimeImage()
-    output_folder = vModel.set.OutputFolder
+    vModel = VertexModelVoronoiFromTimeImage(create_output_folder=False)
     if debugging:
-        load_state(vModel,
-                   os.path.join(PROJECT_DIRECTORY, 'Result/'
-                   '12-05_090050_noise_3.00e-01_bNoise_0.00e+00_lVol_1.00e+00_refV0_1.00e+00_kSubs_1.00e-01_lt_0.00e+00_refA0_9.20e-01_eARBarrier_8.00e-07_RemStiff_0.8_lS1_1.40e+00_lS2_1.40e-02_lS3_1.40e-01_ps_5.00e-05_lc_4.50e-05/'
-                   'data_step_before_remodelling_3157.pkl'))
-        vModel.set.wing_disc()
-        vModel.set.wound_default()
+        output_folder = os.path.join(PROJECT_DIRECTORY, 'Result/final_results_/60_mins_no_Remodelling_no_lateralCablesStrength')
+        # Load last modified pkl file
+        name_last_pkl_file = \
+        sorted([f for f in os.listdir(output_folder) if f.endswith('.pkl') and not 'before_remodelling' in f])[-1]
+        load_state(vModel, os.path.join(output_folder, name_last_pkl_file))
+        #vModel.set.wing_disc()
         #vModel.set.OutputFolder = output_folder
         #vModel.set.update_derived_parameters()
         #os.makedirs(vModel.set.OutputFolder + '/images', exist_ok=True)
         #vModel.save_v_model_state('culete')
-        vModel.tr = 0
-        vModel.iteration_converged()
-        vModel.reset_noisy_parameters()
+        # vModel.tr = 0
+        # vModel.set.redirect_output()
+        # vModel.iteration_converged()
+        # vModel.reset_noisy_parameters()
         vModel.iterate_over_time()
     else:
         load_state(vModel,
