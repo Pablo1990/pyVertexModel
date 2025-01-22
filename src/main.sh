cfacb6e6c28ddd31c53ebd3e4d63bb778f8d078f
Pablo Vicente Munuera <pablovm1990@gmail.com>
2025-01-22 15:57:41 +0000

    main to run in terminal

---
+++ src/main.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Define the Python script to run
+PYTHON_SCRIPT="pyVertexModel/main.py"
+
+# Define the project directory and add it to PYTHONPATH
+PROJECT_DIR=$(dirname "$(dirname "$(realpath $0)")")
+export PYTHONPATH=$PROJECT_DIR:$PYTHONPATH
+
+# Function to run a simulation
+run_simulation() {
+    local num_parameter=$1
+    echo "Running simulation number $num_parameter"
+    python $PYTHON_SCRIPT "$num_parameter"
+    echo "Finished simulation number $num_parameter"
+}
+
+# Run the simulations in parallel with a limit of 5 at a time
+max_jobs=2
+for num_parameter in $(seq 1 $max_jobs); do
+    run_simulation "$num_parameter" &
+    sleep 10s
+done
+wait
