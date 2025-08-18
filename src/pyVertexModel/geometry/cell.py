188d344d56a569b1af1d2d3ce93056737b190342
Pablo Vicente Munuera <pablovm1990@gmail.com>
2025-08-18 06:50:08 +0100

    Temporary fix for collinear points

--- src/pyVertexModel/geometry/cell.py
+++ src/pyVertexModel/geometry/cell.py
@@ -214,9 +214,16 @@ class Cell:
         """
         volumes = np.zeros(len(c_face.Tris))
         for t in range(len(c_face.Tris)):
-            y1 = self.Y[c_face.Tris[t].Edge[0], :] - self.X
-            y2 = self.Y[c_face.Tris[t].Edge[1], :] - self.X
+            idx0 = c_face.Tris[t].Edge[0]
+            idx1 = c_face.Tris[t].Edge[1]
+            y1 = self.Y[idx0, :] - self.X
+            y2 = self.Y[idx1, :] - self.X
             y3 = c_face.Centre - self.X
+
+            if idx0 == idx1 or np.all(y1 == y2):
+                print(
+                    f"Warning: Degenerate triangle with identical edge indices ({idx0}) in cell {self.ID}, face {c_face.globalIds}")
+                continue  # Skip this triangle
 
             current_v = np.linalg.det(np.array([y1, y2, y3])) / 6
             # If the volume is negative, switch two the other option
