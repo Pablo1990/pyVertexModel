5c345300d4cc2bdeaf9dd3955d8207a71a97116b
Pablo Vicente Munuera <pablovm1990@gmail.com>
2025-10-02 10:30:48 +0100

    Changing parameters based on cell shape

--- src/pyVertexModel/main_different_cell_shapes.py
+++ src/pyVertexModel/main_different_cell_shapes.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 import numpy as np
 
@@ -9,48 +10,52 @@ from src.pyVertexModel.analysis.analyse_simulation import analyse_simulation
 original_wing_disc_height = 15.0 # in microns
 set_of_resize_z = np.array([0.0001, 0.001, 0.01, 0.1, 0.5, 1.0, 2.0]) * original_wing_disc_height
 
+set_of_resize_z_to_do = [set_of_resize_z[5]]
+files_done = []
+
 # Get all the files from 'Input/to_rezize/' that end with '.pkl'
 all_files = [f.split('.')[0] for f in os.listdir(PROJECT_DIRECTORY + '/Input/images') if f.endswith('.tif') and not f.endswith('labelled.tif')]
-#all_files.sort()
-for input_file in all_files:
-    for resize_z in set_of_resize_z:
+num_img = int(sys.argv[1])  # Get the image number from command line argument
+input_file = all_files[num_img]  # Use the image file name without extension
+if input_file not in files_done:
+    logger.info(f"Processing file: {input_file}")
+    for resize_z in set_of_resize_z_to_do:
         vModel = VertexModelVoronoiFromTimeImage(create_output_folder=False, set_option='wing_disc')
         vModel.set.initial_filename_state = 'Input/to_resize/' + input_file + '.pkl'
         vModel.set.model_name = input_file + '_' + str(resize_z)
         vModel.set.CellHeight = resize_z
         vModel.set.resize_z = resize_z / original_wing_disc_height
-        vModel.set.myosin_pool = (3e-5 + 7e-5)
+        vModel.set.myosin_pool = 1e-3
+
         # Total surface area considering the number of faces and purse string strength
         if resize_z == set_of_resize_z[0]:
-            lambda_S1 = 0.05
-            lambda_S2 = lambda_S1
-            purse_string_strength = 0.3
-            vModel.set.lambdaV = 0.01
+            lambda_S1 = 0.01
+            lambda_S2 = lambda_S1 * 10
+            purse_string_strength = 0.5
         elif resize_z == set_of_resize_z[1]:
             lambda_S1 = 0.1
             lambda_S2 = lambda_S1
-            purse_string_strength = 0.3
-            #vModel.set.lambdaV = 1
+            purse_string_strength = 0.5
         elif resize_z == set_of_resize_z[2]:
             lambda_S1 = 0.3
             lambda_S2 = lambda_S1
-            purse_string_strength = 0.3
+            purse_string_strength = 0.5
         elif resize_z == set_of_resize_z[3]:
             lambda_S1 = 0.5
             lambda_S2 = lambda_S1/2
-            purse_string_strength = 0.3
+            purse_string_strength = 0.5
         elif resize_z == set_of_resize_z[4]:
             lambda_S1 = 1.0
             lambda_S2 = lambda_S1/30
-            purse_string_strength = 0.3
+            purse_string_strength = 0.5
         elif resize_z == original_wing_disc_height:
             lambda_S1 = 1.4
             lambda_S2 = lambda_S1/100
-            purse_string_strength = 0.3
+            purse_string_strength = 0.5
         elif resize_z == set_of_resize_z[6]:
             lambda_S1 = 1.6
             lambda_S2 = lambda_S1/200
-            purse_string_strength = 0.3
+            purse_string_strength = 0.5
         else:
             logger.info("Resize_z not recognized")
             continue
@@ -65,8 +70,11 @@ for input_file in all_files:
         vModel.set.OutputFolder = None
         vModel.set.update_derived_parameters()
         vModel.set.redirect_output()
+        vModel.set.tend = 21
         try:
             vModel.initialize()
+            # Recompute reference lengths to compare lateral cables and purse string effectively
+            vModel.geo.compute_edge_length_0(default_value=1.0)
             vModel.iterate_over_time()
             analyse_simulation(vModel.set.OutputFolder)
         except Exception as e:
