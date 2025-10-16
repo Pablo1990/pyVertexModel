1e7d1b21ef2a910fc620033935913abd54464a5e
Pablo Vicente Munuera <pablovm1990@gmail.com>
2025-10-16 16:38:00 +0100

    Bugfix on find purse string strength

--- src/pyVertexModel/algorithm/vertexModel.py
+++ src/pyVertexModel/algorithm/vertexModel.py
@@ -1084,24 +1084,24 @@ class VertexModel:
         :param c_folder: Directory to save the results.
         :return:
         """
-        if run_iteration:
+        if run_iteration and self.t < 26:
             self.set.tend = 26  # Run until 20+6 minutes after ablation
             self.iterate_over_time()
 
             # Move files from vModel.set.output_folder to c_folder/ar_dir/directory
-            if self.set.output_folder and os.path.exists(self.set.output_folder):
-                for f in os.listdir(self.set.output_folder):
-                    if os.path.isfile(os.path.join(self.set.output_folder, f)):
-                        os.rename(os.path.join(self.set.output_folder, f), os.path.join(c_folder, ar_dir, directory, f))
-                    elif os.path.isdir(os.path.join(self.set.output_folder, f)):
+            if self.set.OutputFolder and os.path.exists(self.set.OutputFolder):
+                for f in os.listdir(self.set.OutputFolder):
+                    if os.path.isfile(os.path.join(self.set.OutputFolder, f)):
+                        os.rename(os.path.join(self.set.OutputFolder, f), os.path.join(c_folder, ar_dir, directory, f))
+                    elif os.path.isdir(os.path.join(self.set.OutputFolder, f)):
                         # Merge subdirectories
-                        sub_dir = os.path.join(self.set.output_folder, f)
+                        sub_dir = os.path.join(self.set.OutputFolder, f)
                         dest_sub_dir = os.path.join(c_folder, ar_dir, directory, f)
                         if not os.path.exists(dest_sub_dir):
                             os.makedirs(dest_sub_dir)
                         for sub_f in os.listdir(sub_dir):
                             os.rename(os.path.join(sub_dir, sub_f), os.path.join(dest_sub_dir, sub_f))
-                # os.rmdir(vModel.set.output_folder)
+                # os.rmdir(vModel.set.OutputFolder)
 
         # Save the state before starting the purse string strength exploration as backup
         backup_vars = save_backup_vars(self.geo, self.geo_n, self.geo_0, self.tr, self.Dofs)
