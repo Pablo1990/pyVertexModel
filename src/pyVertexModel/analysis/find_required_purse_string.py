dc8a3d9c76ac3174bfd1847176ecb394cacbbfe6
Pablo Vicente Munuera <pablovm1990@gmail.com>
2025-10-16 15:01:17 +0100

    run it also just once

--- src/pyVertexModel/analysis/find_required_purse_string.py
+++ src/pyVertexModel/analysis/find_required_purse_string.py
@@ -5,26 +5,37 @@ from src import PROJECT_DIRECTORY
 from src.pyVertexModel.algorithm.vertexModelVoronoiFromTimeImage import VertexModelVoronoiFromTimeImage
 from src.pyVertexModel.util.utils import load_state
 
-# Folder containing different simulations with different cell shapes
-c_folder = os.path.join(PROJECT_DIRECTORY, 'Result/different_cell_shape_healing/')
+single_file = True
 
-# Get all directories within c_folder
-all_directories = os.listdir(c_folder)
-all_directories = [d for d in all_directories if os.path.isdir(os.path.join(c_folder, d))]
-all_directories.sort()
+if single_file:
+    c_folder = os.path.join(PROJECT_DIRECTORY, 'Result/single_simulation_healing/')
+    ar_dir = ''
+    directory = ''
+    file_name = ''
+    vModel = VertexModelVoronoiFromTimeImage(create_output_folder=False, set_option='wing_disc_equilibrium')
+    load_state(vModel, os.path.join(c_folder, ar_dir, directory, file_name))
+    vModel.required_purse_string_strength(directory, ar_dir, c_folder)
+else:
+    # Folder containing different simulations with different cell shapes
+    c_folder = os.path.join(PROJECT_DIRECTORY, 'Result/different_cell_shape_healing/')
 
-for ar_dir in all_directories:
-    simulations_dirs = os.listdir(os.path.join(c_folder, ar_dir))
-    simulations_dirs = [d for d in simulations_dirs if os.path.isdir(os.path.join(c_folder, ar_dir, d))]
-    for directory in simulations_dirs:
-        # Get t=6 or more minutes after ablation, but the closest to 6 minutes
-        files_within_folder = os.listdir(os.path.join(c_folder, ar_dir, directory))
-        files_ending_pkl = [f for f in files_within_folder if f.endswith('.pkl') and f.startswith('data_step_')]
+    # Get all directories within c_folder
+    all_directories = os.listdir(c_folder)
+    all_directories = [d for d in all_directories if os.path.isdir(os.path.join(c_folder, d))]
+    all_directories.sort()
 
-        # Sort files_ending_pkl by the time in the filename
-        files_ending_pkl.sort(key=lambda x: int(x.split('_')[2].split('.')[0]))
+    for ar_dir in all_directories:
+        simulations_dirs = os.listdir(os.path.join(c_folder, ar_dir))
+        simulations_dirs = [d for d in simulations_dirs if os.path.isdir(os.path.join(c_folder, ar_dir, d))]
+        for directory in simulations_dirs:
+            # Get t=6 or more minutes after ablation, but the closest to 6 minutes
+            files_within_folder = os.listdir(os.path.join(c_folder, ar_dir, directory))
+            files_ending_pkl = [f for f in files_within_folder if f.endswith('.pkl') and f.startswith('data_step_')]
 
-        # Load it
-        vModel = VertexModelVoronoiFromTimeImage(create_output_folder=False, set_option='wing_disc_equilibrium')
-        load_state(vModel, os.path.join(c_folder, ar_dir, directory, files_ending_pkl[-1]))
-        vModel.required_purse_string_strength(directory, ar_dir, c_folder)
+            # Sort files_ending_pkl by the time in the filename
+            files_ending_pkl.sort(key=lambda x: int(x.split('_')[2].split('.')[0]))
+
+            # Load it
+            vModel = VertexModelVoronoiFromTimeImage(create_output_folder=False, set_option='wing_disc_equilibrium')
+            load_state(vModel, os.path.join(c_folder, ar_dir, directory, files_ending_pkl[-1]))
+            vModel.required_purse_string_strength(directory, ar_dir, c_folder)
