7d7e8e3f9350d84667e613d05a47b098fbb36bce
Pablo Vicente Munuera <pablovm1990@gmail.com>
2025-12-17 10:52:52 +0000

    paralelising it

--- src/pyVertexModel/analysis/find_required_purse_string.py
+++ src/pyVertexModel/analysis/find_required_purse_string.py
@@ -22,114 +22,126 @@ else:
     # Folder containing different simulations with different cell shapes
     c_folder = os.path.join(PROJECT_DIRECTORY, 'Result/to_calculate_ps_recoil/')
     output_csv = os.path.join(PROJECT_DIRECTORY, 'Result/to_calculate_ps_recoil/required_purse_string_strengths.csv')
-    if not os.path.exists(output_csv):
-        # Get all directories within c_folder
-        all_directories = os.listdir(c_folder)
-        all_directories = [d for d in all_directories if os.path.isdir(os.path.join(c_folder, d))]
+    #if not os.path.exists(output_csv):
+    # Get all directories within c_folder
+    all_directories = os.listdir(c_folder)
+    all_directories = [d for d in all_directories if os.path.isdir(os.path.join(c_folder, d))]
 
-        # Save ps_strengths and dy for each cell shape
-        aspect_ratio = []
-        recoilings = []
-        time_list = []
-        purse_string_strength_0 = []
-        lambda_s1_list = []
-        lambda_s2_list = []
-        model_name = []
-        for ar_dir in all_directories:
-            simulations_dirs = os.listdir(os.path.join(c_folder, ar_dir))
-            simulations_dirs = [d for d in simulations_dirs if os.path.isdir(os.path.join(c_folder, ar_dir, d))]
-            # Sort descending order
-            simulations_dirs.sort(reverse=True)
-            for directory in simulations_dirs:
-                print(f"Processing directory: {directory}")
+    # Save ps_strengths and dy for each cell shape
+    aspect_ratio = []
+    recoilings = []
+    time_list = []
+    purse_string_strength_0 = []
+    lambda_s1_list = []
+    lambda_s2_list = []
+    model_name = []
+    for ar_dir in all_directories:
+        simulations_dirs = os.listdir(os.path.join(c_folder, ar_dir))
+        simulations_dirs = [d for d in simulations_dirs if os.path.isdir(os.path.join(c_folder, ar_dir, d))]
 
-                # Get the purse string strength
-                vModel = VertexModelVoronoiFromTimeImage(create_output_folder=False, set_option='wing_disc')
+        if len(simulations_dirs) == 0:
+            print(f"No simulation directories found in {ar_dir}, skipping.")
+            continue
 
-                files_within_folder = os.listdir(os.path.join(c_folder, ar_dir, directory))
-                if 'before_ablation.pkl' not in files_within_folder:
-                    print(f"Skipping {directory} as 'before_ablation.pkl' not found.")
-                    continue
+        directory = simulations_dirs[int(sys.argv[1])]
+        #for directory in simulations_dirs:
+        print(f"Processing directory: {directory}")
 
-                load_state(vModel, os.path.join(c_folder, ar_dir, directory, 'before_ablation.pkl'))
-                t_ablation = vModel.t
+        # Get the purse string strength
+        vModel = VertexModelVoronoiFromTimeImage(create_output_folder=False, set_option='wing_disc')
 
-                # Run the required purse string strength analysis
-                current_folder = vModel.set.OutputFolder
-                last_folder = current_folder.split('/')
-                vModel.set.OutputFolder = os.path.join(PROJECT_DIRECTORY, 'Result/', last_folder[-1])
-                _, _, recoiling, purse_string_strength_eq = vModel.required_purse_string_strength(
-                    os.path.join(c_folder, ar_dir, directory), tend=t_ablation + 0.1)
+        files_within_folder = os.listdir(os.path.join(c_folder, ar_dir, directory))
+        if 'before_ablation.pkl' not in files_within_folder:
+            print(f"Skipping {directory} as 'before_ablation.pkl' not found.")
+            continue
 
-                recoilings.append(recoiling)
-                purse_string_strength_0.append(purse_string_strength_eq)
-                aspect_ratio.append(vModel.set.CellHeight)
-                lambda_s1_list.append(vModel.set.lambdaS1)
-                lambda_s2_list.append(vModel.set.lambdaS2)
-                model_name.append(vModel.set.model_name)
-                time_list.append(0.1)
+        load_state(vModel, os.path.join(c_folder, ar_dir, directory, 'before_ablation.pkl'))
+        t_ablation = vModel.t
 
-                vModel.set.OutputFolder = os.path.join(PROJECT_DIRECTORY, 'Result/', last_folder[-1])
-                _, _, recoiling_t_6, purse_string_strength_eq_t_6 = vModel.required_purse_string_strength(
-                    os.path.join(c_folder, ar_dir, directory), tend=t_ablation + 6.0)
+        # Run the required purse string strength analysis
+        current_folder = vModel.set.OutputFolder
+        last_folder = current_folder.split('/')
+        vModel.set.OutputFolder = os.path.join(PROJECT_DIRECTORY, 'Result/', last_folder[-1])
+        _, _, recoiling, purse_string_strength_eq = vModel.required_purse_string_strength(
+            os.path.join(c_folder, ar_dir, directory), tend=t_ablation + 0.1)
 
-                recoilings.append(recoiling_t_6)
-                purse_string_strength_0.append(purse_string_strength_eq_t_6)
-                aspect_ratio.append(vModel.set.CellHeight)
-                lambda_s1_list.append(vModel.set.lambdaS1)
-                lambda_s2_list.append(vModel.set.lambdaS2)
-                model_name.append(vModel.set.model_name)
-                time_list.append(6.0)
-                analyse_simulation(os.path.join(c_folder, ar_dir, directory))
+        recoilings.append(recoiling)
+        purse_string_strength_0.append(purse_string_strength_eq)
+        aspect_ratio.append(vModel.set.CellHeight)
+        lambda_s1_list.append(vModel.set.lambdaS1)
+        lambda_s2_list.append(vModel.set.lambdaS2)
+        model_name.append(vModel.set.model_name)
+        time_list.append(0.1)
 
-        # Append results into an existing csv file
+        vModel.set.OutputFolder = os.path.join(PROJECT_DIRECTORY, 'Result/', last_folder[-1])
+        _, _, recoiling_t_6, purse_string_strength_eq_t_6 = vModel.required_purse_string_strength(
+            os.path.join(c_folder, ar_dir, directory), tend=t_ablation + 6.0)
+
+        recoilings.append(recoiling_t_6)
+        purse_string_strength_0.append(purse_string_strength_eq_t_6)
+        aspect_ratio.append(vModel.set.CellHeight)
+        lambda_s1_list.append(vModel.set.lambdaS1)
+        lambda_s2_list.append(vModel.set.lambdaS2)
+        model_name.append(vModel.set.model_name)
+        time_list.append(6.0)
+        analyse_simulation(os.path.join(c_folder, ar_dir, directory))
+
+    # Append results into an existing csv file
+    if os.path.exists(output_csv):
+        df_existing = pd.read_csv(output_csv)
+        df = pd.DataFrame(
+            {'Model_name': model_name, 'AR': aspect_ratio, 'LambdaS1': lambda_s1_list, 'LambdaS2': lambda_s2_list,
+             'Recoil': recoilings, 'Purse_string_strength': purse_string_strength_0, 'time': time_list})
+        df_final = pd.concat([df_existing, df], ignore_index=True)
+        df_final.to_csv(output_csv, index=False)
+    else:
         df = pd.DataFrame(
             {'Model_name': model_name, 'AR': aspect_ratio, 'LambdaS1': lambda_s1_list, 'LambdaS2': lambda_s2_list,
              'Recoil': recoilings, 'Purse_string_strength': purse_string_strength_0, 'time': time_list})
         df.to_csv(output_csv, index=False)
-    else:
-        df = pd.read_csv(output_csv)
+    #else:
+    #    df = pd.read_csv(output_csv)
 
-    # Check if df contains any nans or infs
-    if df.isnull().values.any():
-        print("DataFrame contains NaN values. Please check the data.")
-
-        # Remove rows with NaN values
-        df = df.dropna()
-
-    if df.isin([float('inf'), float('-inf')]).values.any():
-        print("DataFrame contains infinite values. Please check the data.")
-
-        # Remove rows with infinite values
-        df = df[~df.isin([float('inf'), float('-inf')]).any(axis=1)]
-
-    # Two timepoints: 0.1 and 6.0 minutes after ablation
-    for timepoint in [0.1, 6.0]:
-        df_time = df[df['time'] == timepoint]
-
-        # Keep recoil values that are close to 2.4e-13 +- 4e-14
-        if timepoint == 0.1:
-            df_time = df_time[(df_time['Recoil'] > 2.0e-13) & (df_time['Recoil'] < 3e-13)]
-
-        # Save the cleaned DataFrame with 'filtered' suffix
-        df_time.to_csv(os.path.join(PROJECT_DIRECTORY, 'Result', 'to_calculate_ps_recoil', 'required_purse_string_strengths_filtered' + f'_time_{timepoint}.csv'), index=False)
-
-        # Plot recoil over aspect ratio
-        plot_figure_with_line(df_time, None, os.path.join(PROJECT_DIRECTORY, 'Result', 'to_calculate_ps_recoil', str(timepoint)),
-                              x_axis_name='AR',
-                              y_axis_name='Recoil', y_axis_label='Recoil velocity (dt=1e-10)')
-
-        plot_figure_with_line(df_time, None, os.path.join(PROJECT_DIRECTORY, 'Result', 'to_calculate_ps_recoil', str(timepoint)),
-                              x_axis_name='AR',
-                              y_axis_name='Purse_string_strength',
-                              y_axis_label='Min. purse string strength')
-
-        plot_figure_with_line(df_time, None, os.path.join(PROJECT_DIRECTORY, 'Result', 'to_calculate_ps_recoil', str(timepoint)),
-                              x_axis_name='AR',
-                              y_axis_name='LambdaS1',
-                              y_axis_label=r'$\lambda_{s1}=\lambda_{s3}$')
-
-        plot_figure_with_line(df_time, None, os.path.join(PROJECT_DIRECTORY, 'Result', 'to_calculate_ps_recoil', str(timepoint)),
-                              x_axis_name='AR',
-                              y_axis_name='LambdaS2',
-                              y_axis_label=r'$\lambda_{s2}$')
+    # # Check if df contains any nans or infs
+    # if df.isnull().values.any():
+    #     print("DataFrame contains NaN values. Please check the data.")
+    #
+    #     # Remove rows with NaN values
+    #     df = df.dropna()
+    #
+    # if df.isin([float('inf'), float('-inf')]).values.any():
+    #     print("DataFrame contains infinite values. Please check the data.")
+    #
+    #     # Remove rows with infinite values
+    #     df = df[~df.isin([float('inf'), float('-inf')]).any(axis=1)]
+    #
+    # # Two timepoints: 0.1 and 6.0 minutes after ablation
+    # for timepoint in [0.1, 6.0]:
+    #     df_time = df[df['time'] == timepoint]
+    #
+    #     # Keep recoil values that are close to 2.4e-13 +- 4e-14
+    #     if timepoint == 0.1:
+    #         df_time = df_time[(df_time['Recoil'] > 2.0e-13) & (df_time['Recoil'] < 3e-13)]
+    #
+    #     # Save the cleaned DataFrame with 'filtered' suffix
+    #     df_time.to_csv(os.path.join(PROJECT_DIRECTORY, 'Result', 'to_calculate_ps_recoil', 'required_purse_string_strengths_filtered' + f'_time_{timepoint}.csv'), index=False)
+    #
+    #     # Plot recoil over aspect ratio
+    #     plot_figure_with_line(df_time, None, os.path.join(PROJECT_DIRECTORY, 'Result', 'to_calculate_ps_recoil', str(timepoint)),
+    #                           x_axis_name='AR',
+    #                           y_axis_name='Recoil', y_axis_label='Recoil velocity (dt=1e-10)')
+    #
+    #     plot_figure_with_line(df_time, None, os.path.join(PROJECT_DIRECTORY, 'Result', 'to_calculate_ps_recoil', str(timepoint)),
+    #                           x_axis_name='AR',
+    #                           y_axis_name='Purse_string_strength',
+    #                           y_axis_label='Min. purse string strength')
+    #
+    #     plot_figure_with_line(df_time, None, os.path.join(PROJECT_DIRECTORY, 'Result', 'to_calculate_ps_recoil', str(timepoint)),
+    #                           x_axis_name='AR',
+    #                           y_axis_name='LambdaS1',
+    #                           y_axis_label=r'$\lambda_{s1}=\lambda_{s3}$')
+    #
+    #     plot_figure_with_line(df_time, None, os.path.join(PROJECT_DIRECTORY, 'Result', 'to_calculate_ps_recoil', str(timepoint)),
+    #                           x_axis_name='AR',
+    #                           y_axis_name='LambdaS2',
+    #                           y_axis_label=r'$\lambda_{s2}$')
