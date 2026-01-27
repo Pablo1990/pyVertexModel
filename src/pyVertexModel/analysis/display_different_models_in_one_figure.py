a10f335b3e209ba36d0a7ae45495243069532e32
Pablo Vicente Munuera <pablovm1990@gmail.com>
2026-01-27 10:41:15 +0000

    dispalying whole tissue

--- src/pyVertexModel/analysis/display_different_models_in_one_figure.py
+++ src/pyVertexModel/analysis/display_different_models_in_one_figure.py
@@ -13,6 +13,7 @@ import matplotlib.pyplot as plt
 
 # files to load
 folder = '/media/pablo/d7c61090-024c-469a-930c-f5ada47fb049/PabloVicenteMunuera/VertexModel/pyVertexModel/Input/images/'
+output_folder = '/media/pablo/d7c61090-024c-469a-930c-f5ada47fb049/PabloVicenteMunuera/VertexModel/pyVertexModel/Result/'
 files_to_load = ['dWP1_150cells_0.0015.pkl', 'dWP1_150cells_0.015.pkl','dWP1_150cells_0.15.pkl', 'dWP1_150cells_1.5.pkl', 'dWP1_150cells_7.5.pkl', 'dWP1_150cells_15.0.pkl', 'dWP1_150cells_30.0.pkl']
 
 geo = Geo()
@@ -33,15 +34,13 @@ for id_file, file in enumerate(files_to_load):
             mesh = cell.create_pyvista_mesh(offset=offset)
 
             # Add the mesh to the plotter
-            plotter.add_mesh(mesh, name=f'cell_{cell.ID}_{file}', scalars='ID', lighting=True, cmap="tab10",
+            plotter.add_mesh(mesh, name=f'cell_{cell.ID}_{file}', scalars='ID', lighting=True, cmap="hot",
                              clim=colormap_lim, show_edges=True, edge_color='white', edge_opacity=0.3)
-            break
     for _, cell in enumerate(geo.Cells):
         if cell.AliveStatus == 1:
             edge_mesh = cell.create_pyvista_edges(offset=offset)
             plotter.add_mesh(edge_mesh, name=f'edge_{cell.ID}_{file}', color='black', line_width=3,
                              render_lines_as_tubes=True)
-            break
 
 #plotter.enable_parallel_projection()
 plotter.enable_image_style()
@@ -65,5 +64,5 @@ plotter.close()
 fig = plt.figure(figsize=(10, 10), dpi=2000)
 plt.imshow(img)
 plt.axis('off')
-plt.savefig(folder + 'dWP1_cell_shape_1_cell.png', bbox_inches='tight')
+plt.savefig(output_folder + 'dWP1_all_tissues.png', bbox_inches='tight')
 plt.close(fig)
