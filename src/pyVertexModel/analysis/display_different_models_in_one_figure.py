fbd65ef59dc9aa3ba75b4bb6c68ab3b7c4056b97
Pablo Vicente Munuera <pablovm1990@gmail.com>
2026-01-27 11:11:29 +0000

    displaying all cells

--- src/pyVertexModel/analysis/display_different_models_in_one_figure.py
+++ src/pyVertexModel/analysis/display_different_models_in_one_figure.py
@@ -14,7 +14,7 @@ import matplotlib.pyplot as plt
 # files to load
 folder = '/media/pablo/d7c61090-024c-469a-930c-f5ada47fb049/PabloVicenteMunuera/VertexModel/pyVertexModel/Input/images/'
 output_folder = '/media/pablo/d7c61090-024c-469a-930c-f5ada47fb049/PabloVicenteMunuera/VertexModel/pyVertexModel/Result/'
-files_to_load = ['dWP1_150cells_0.0015.pkl', 'dWP1_150cells_0.015.pkl','dWP1_150cells_0.15.pkl', 'dWP1_150cells_1.5.pkl', 'dWP1_150cells_7.5.pkl', 'dWP1_150cells_15.0.pkl', 'dWP1_150cells_30.0.pkl']
+files_to_load = ['dWP1_150cells_0.15.pkl', 'dWP1_150cells_1.5.pkl', 'dWP1_150cells_7.5.pkl', 'dWP1_150cells_15.0.pkl', 'dWP1_150cells_30.0.pkl']
 
 geo = Geo()
 
@@ -23,19 +23,23 @@ plotter = pv.Plotter(off_screen=True)
 
 colormap_lim = [0.0001, 0.0006]
 
+cells_to_hide = [1, 5, 14, 23, 35]
+
+# Set an offset for each model to avoid overlap
+offset = np.array([0.0, 0.0, 0.0])
 for id_file, file in enumerate(files_to_load):
     load_state(geo, folder + file)
     geo.resize_tissue()
-    # Set an offset for each model to avoid overlap
-    offset = np.array([0.5, 0.0, 0.0]) * id_file
     for _, cell in enumerate(geo.Cells):
         if cell.AliveStatus == 1:
             # Load the VTK file as a pyvista mesh
             mesh = cell.create_pyvista_mesh(offset=offset)
 
             # Add the mesh to the plotter
-            plotter.add_mesh(mesh, name=f'cell_{cell.ID}_{file}', scalars='ID', lighting=True, cmap="hot",
+            plotter.add_mesh(mesh, name=f'cell_{cell.ID}_{file}', scalars='Volume', lighting=True, cmap="pink",
                              clim=colormap_lim, show_edges=True, edge_color='white', edge_opacity=0.3)
+
+
     for _, cell in enumerate(geo.Cells):
         if cell.AliveStatus == 1:
             edge_mesh = cell.create_pyvista_edges(offset=offset)
@@ -43,6 +47,10 @@ for id_file, file in enumerate(files_to_load):
                              render_lines_as_tubes=True)
 
 #plotter.enable_parallel_projection()
+    bounds = plotter.bounds
+    x_size = bounds[1] - bounds[0]
+    offset = np.array([x_size, 0.0, 0.0])
+
 plotter.enable_image_style()
 plotter.view_xz()
 
