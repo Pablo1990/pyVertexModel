9f26af9dd3bfd65a0e23ae51ee47b90e4bbf9664
Pablo Vicente Munuera <pablovm1990@gmail.com>
2026-02-02 12:10:25 +0100

    tests are not running

--- Tests/__init__.py
+++ Tests/__init__.py
@@ -1,3 +1,29 @@
+import logging
 import os
+import warnings
 
 TEST_DIRECTORY = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+PROJECT_DIRECTORY = os.getenv('PROJECT_DIR', os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+# get the logger instance
+logger = logging.getLogger("pyVertexModel")
+
+formatter = logging.Formatter(
+    "%(levelname)s [%(asctime)s] pyVertexModel: %(message)s",
+    datefmt="%Y/%m/%d %I:%M:%S %p",
+)
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(formatter)
+logger.addHandler(console_handler)
+
+logger.setLevel(logging.DEBUG)
+logger.propagate = False
+
+
+# Function to handle warnings
+def warning_handler(message, category, filename, lineno, file=None, line=None):
+    logger.warning(f'{filename}:{lineno}: {category.__name__}: {message}')
+
+# Set the warnings' showwarning function to the handler
+warnings.showwarning = warning_handler
