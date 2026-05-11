37459b9d38235d6b3f663ebb353638cbcece044c
Pablo Vicente Munuera <pablovm1990@gmail.com>
2026-05-11 09:55:22 +0100

    improved

--- src/pyVertexModel/analysis/analyse_simulations.py
+++ src/pyVertexModel/analysis/analyse_simulations.py
@@ -4,12 +4,12 @@ import numpy as np
 import pandas as pd
 
 from pyVertexModel.analysis.analyse_simulation import analyse_simulation, create_video
-from pyVertexModel.util.utils import plot_figure_with_line
+from pyVertexModel.util.utils import plot_figure_with_line, plot_feature_over_time
 
-folder = '/media/pablo/d7c61090-024c-469a-930c-f5ada47fb049/PabloVicenteMunuera/VertexModel/pyVertexModel/Result/same_recoil_wound_area_results/c/'
-
+folder = '/media/pablo/d7c61090-024c-469a-930c-f5ada47fb049/PabloVicenteMunuera/VertexModel/pyVertexModel/Result/to_calculate_ps_recoil/c/' #same_recoil_wound_area_results #to_calculate_ps_recoil
+output_file = os.path.join(folder, 'all_files_features_same_number_of_cells.xlsx') #all_files_features_same_area_ablating
 # Check if excel exists
-if not os.path.exists(os.path.join(folder, 'all_files_features.xlsx')):
+if not os.path.exists(output_file):
     print("Excel file does not exist. Creating it...")
 
     all_files_features = []
@@ -26,7 +26,7 @@ if not os.path.exists(os.path.join(folder, 'all_files_features.xlsx')):
         print(file)
 
         # if file is a directory
-        if os.path.isdir(os.path.join(folder, file)):
+        if os.path.isdir(os.path.join(folder, file)) and not file.__contains__('ablating') and not file.__contains__('not_used'):
             files_within_folder = os.listdir(os.path.join(folder, file))
             # Analyse the edge recoil
             if os.path.exists(os.path.join(folder, file, 'before_ablation.pkl')):
@@ -52,7 +52,7 @@ if not os.path.exists(os.path.join(folder, 'all_files_features.xlsx')):
                         #print("Error cells_area_top_lower_than_average_diff: ", e)
                         important_features['cells_area_top_lower_than_average_diff'] = np.nan
 
-                    if file.endswith('no_Remodelling'):
+                    if folder.endswith('/c/') and file.__contains__('no_Remodelling'):
                         file = file.split('/')[-2]
                     else:
                         file = file.split('/')[-1]
@@ -67,8 +67,15 @@ if not os.path.exists(os.path.join(folder, 'all_files_features.xlsx')):
                         if file_splitted[i] in variables_to_show:
                             important_features[file_splitted[i]] = file_splitted[i + 1]
 
-                    important_features['AR'] = float(file_splitted[3])  # Aspect Ratio
-                    important_features['model'] = file_splitted[2]  # Model name
+                    # Check if file_splitted[3] is a number, if not, set it to 15.0 and model to 'paper'
+
+                    try:
+                        important_features['AR'] = float(file_splitted[3])  # Aspect Ratio
+                        important_features['model'] = file_splitted[2]  # Model name
+                    except Exception as e:
+                        print("Error parsing AR and model: ", e)
+                        important_features['AR'] = 15.0
+                        important_features['model'] = 'paper'
 
                     important_features['top_closure_velocity'] = (important_features['max_recoiling_top'] - important_features['last_area_top']) / important_features['last_area_time_top']
 
@@ -81,12 +88,16 @@ if not os.path.exists(os.path.join(folder, 'all_files_features.xlsx')):
 
     # Export to xls file
     df = pd.DataFrame(all_files_features)
-    df.to_excel(os.path.join(folder, 'all_files_features.xlsx'))
+    df.to_excel(output_file)
 else:
     print("Excel file exists. Loading it...")
-    df = pd.read_excel(os.path.join(folder, 'all_files_features.xlsx'), header=0)
+    df = pd.read_excel(output_file, header=0)
 
 # Plot figures
 df_filtered = df[df['last_area_time_top'] > 20.0]
 plot_figure_with_line(df_filtered, None, folder, y_axis_name='top_closure_velocity', y_axis_label='Apical closure velocity', x_axis_name='AR')
 plot_figure_with_line(df_filtered, None, folder, y_axis_name='max_recoiling_top', y_axis_label='Apical max recoiling', x_axis_name='AR')
+
+# Plot volume
+plot_feature_over_time(df, 'Volume_wound_edge_extrapolated', y_axis_label='Wound edge cell volume (%)', current_path=folder)
+plot_feature_over_time(df, 'Volume_extrapolated', y_axis_label='Cell volume (%)', current_path=folder)
